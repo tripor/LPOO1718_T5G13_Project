a00fb7f5314e3db0c2ed74d9c959160d0f051044
João Lima <figorifico3000@gmail.com>
2018-04-18 18:36:01 +0100

    libgdx

---
+++ Ground Up/desktop/src/com/gu/ground/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gu.ground.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gu.ground.GroundUp;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GroundUp(), config);
+	}
+}
