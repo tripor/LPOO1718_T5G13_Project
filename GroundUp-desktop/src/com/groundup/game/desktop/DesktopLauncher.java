ed1fcf8a1172433889c2b25528d680717a5a9edb
João Lima <figorifico3000@gmail.com>
2018-04-18 19:25:15 +0100

    fix project 2

---
+++ GroundUp-desktop/src/com/groundup/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.groundup.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.groundup.game.GroundUp;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GroundUp(), config);
+	}
+}
