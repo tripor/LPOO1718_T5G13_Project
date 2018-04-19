b599f6e04317d02a608a43a7c10d4046a2e06e5d
João Lima <figorifico3000@gmail.com>
2018-04-19 10:21:36 +0100

    basic

--- GroundUp-desktop/src/com/groundup/game/desktop/DesktopLauncher.java
+++ GroundUp-desktop/src/com/groundup/game/desktop/DesktopLauncher.java
@@ -2,11 +2,12 @@ package com.groundup.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.groundup.game.GroundUp;
+
+import graphic.GroundUpGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new GroundUp(), config);
+		new LwjglApplication(new GroundUpGame(), config);
 	}
 }
