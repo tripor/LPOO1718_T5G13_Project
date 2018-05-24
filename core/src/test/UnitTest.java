171a79272957a4907f5a78d957d92ba7fb8d54c0
Johnson Ho <hellokcho@gmail.com>
2018-05-24 11:27:48 +0100

    Start Testing Yeah~

---
+++ core/src/test/UnitTest.java
@@ -0,0 +1,16 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UnitTest {
+	
+	GameStage game;
+
+	@Test
+	void testNewGame() {
+		fail("Not yet implemented");
+	}
+
+}
